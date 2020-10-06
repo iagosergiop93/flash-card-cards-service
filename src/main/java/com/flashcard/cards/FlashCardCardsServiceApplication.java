e306fcc90507cf2eb0c02cabb8ae139d6a66f16b
iagosergiop93 <iagosergiop93@gmail.com>
2020-10-06 13:24:32 -0400

    First commit

---
+++ src/main/java/com/flashcard/cards/FlashCardCardsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flashcard.cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashCardCardsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashCardCardsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flashcard/cards/FlashCardCardsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashcard.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashCardCardsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
